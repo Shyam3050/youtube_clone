be485d23a36ffc680196cfbe984aee18896b7d08
Shyam3050 <nimmanashyam3050@gmail.com>
2023-10-27 19:54:04 +0530

    updated

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,4 +1,7 @@
-// import "firebase/compat/auth";
+// import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -10,6 +13,6 @@ const firebaseConfig = {
   appId: "1:519157297377:web:919bb31182020492e9c7a0",
 };
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 export default auth;
--- src/store/reducers/authentication.jsx
+++ src/store/reducers/authentication.jsx
@@ -1,24 +1,23 @@
 import auth from "../../firebase";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 export const authentication = createAsyncThunk(
   "youtube-clone/authentication",
   async () => {
     try {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      const res = await auth.signInWithPopup(provider);
+      const provider = new GoogleAuthProvider();
+      const res = await signInWithPopup(auth, provider);
       const accessToken = res.credential.accessToken;
       const profile = {
         name: res.additionalUserInfo.profile.name,
         profilePictureUrl: res.additionalUserInfo.profile.picture,
       };
-      
+
       return {
         accessToken: accessToken,
         user: profile,
       };
     } catch (error) {
-      
       console.log(error);
     }
   }
